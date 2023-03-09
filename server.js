202c0250821f57a5eab17b87c8394ba5f3f6641e
مهدی دهقانی <dehghani.m.c@gmail.com>
2023-03-09 09:46:55 +0330

    .

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 'use strict';
+
 const express = require('express');
-const socketIO = require('socket.io');
+const socket = require('socket.io');
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+
 const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .use((req, res) => res.sendFile('/index.html', { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
-const io = require("socket.io")(server, {
+
+const io = socket(server, {
     cors: {
         origins: "*:*",
         methods: ["GET", "POST"]
     }
 });
+
 io.on('connection', (socket) => {
     socket.on('disconnect', () => console.log('Client disconnected'));
-    socket.on('messaged', (args) => {
-        io.emit('message', args);
-        console.log(args)
+
+    socket.on('offer', payload => {
+        io.emit('message', { payload, type: 'offer' });
+        console.log({ offer: payload });
     });
-    socket.on('event_name', (...args) => {
-        io.emit('message2', args);
-        console.log(args)
+
+    socket.on('answer', payload => {
+        io.emit('message', { payload, type: 'answer' });
+        console.log({ answer: payload });
     });
 });
