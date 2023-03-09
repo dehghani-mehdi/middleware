89ed865108aef7ab2c6157e9db1728519c6ec06a
مهدی دهقانی <dehghani.m.c@gmail.com>
2023-03-09 10:03:57 +0330

    .

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-'use strict';
-
-const express = require('express');
-const socket = require('socket.io');
+const WebSocket = require('ws');
 const PORT = process.env.PORT || 3000;
+const wss = new WebSocket.Server({ port: PORT });
 
-const server = express()
-    .use((req, res) => res.sendFile('/index.html', { root: __dirname }))
-    .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socket(server, {
-    cors: {
-        origins: "*:*",
-        methods: ["GET", "POST"]
-    }
-});
-
-io.on('connection', (socket) => {
-    socket.on('disconnect', () => console.log('Client disconnected'));
-
-    socket.on('offer', payload => {
-        io.emit('message', { payload, type: 'offer' });
-        console.log({ offer: payload });
-    });
-
-    socket.on('answer', payload => {
-        io.emit('message', { payload, type: 'answer' });
-        console.log({ answer: payload });
+wss.on('connection', ws => {
+    ws.on('message', payload => {
+        console.log(`Received message => ${payload}`)
+        ws.send(payload.toString());
     });
 });
